daa900cb6a54d0eb2dc7c070d4f320ec01b01848
Marco Faustinelli <marco.faustinelli@edenred.com>
2025-07-03 08:45:28 +0200

    simple reduce object example

--- javascript/02_reduce/reduce.js
+++ javascript/02_reduce/reduce.js
@@ -10,28 +10,40 @@ function complexUrlAnalyzer() {
 
 // https://www.100jsfunctions.com/exercises/arrayToObject
 function arrayToObject(strings) {
-    const result = strings
-      .reduce((acc, curr) => {
-        acc = {
-            ...acc,
-            [curr]: strings.indexOf(curr),
-        };
-        return acc;
-      }, {});
-    return result;
+  const result = strings
+    .reduce((acc, curr) => {
+      acc = {
+        ...acc,
+        [curr]: strings.indexOf(curr),
+      };
+      return acc;
+    }, {});
+  return result;
 }
 
 // https://www.100jsfunctions.com/exercises/pickFields
 function pickFields(data, fields) {
-    return fields.reduce((acc, curr) => {
-        acc = data[curr]
-            ? {
-                ...acc,
-                [curr]: data[curr],
-            }
-            : acc;
-        return acc;
-    }, {})
+  return fields.reduce((acc, curr) => {
+    acc = (data[curr])
+      ? {
+        ...acc,
+        [curr]: data[curr],
+      }
+      : acc;
+    return acc;
+  }, {})
+}
+
+// ['ciao','mamma','ciao'] -> { ciao:1 } -> { ciao:1, mamma:1 } -> { ciao:2, mamma:1 }
+function xxxx(arra) {
+  return arra.reduce((acc, curr) => {
+    return {
+      ...acc,
+      [curr]: (acc[curr])
+        ? acc[curr] + 1
+        : 1,
+    }
+  }, {});
 }
 
 
