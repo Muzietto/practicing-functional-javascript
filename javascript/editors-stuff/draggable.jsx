f192c8d53da8a6ed5258905248d664822572e5e0
Marco Faustinelli <marco.faustinelli@edenred.com>
2025-07-14 18:43:03 +0200

    improved jsx stuff

--- javascript/editors-stuff/draggable.jsx
+++ javascript/editors-stuff/draggable.jsx
@@ -1,29 +1,29 @@
 <MultipleEntryReferenceEditor
-sdk={sdk}
-hasCardEditActions
-hasCardMoveActions
-parameters={{ instance: {
-  showCreateEntityAction: true,
-  showLinkEntityAction: true,
-} }}
-renderCustomCard={(props, linkActionsProps, renderDefaultCard) => {
-  return (props.entity.fields.checkboxChecked)
-? <Card 
-testId={`custom-card_${Math.random()}`}
-// you need both dragHandleRender & withDragHandle here
-dragHandleRender={props.renderDragHandle}
-withDragHandle
->
-  <Paragraph>{props.entity.fields.name['it-IT']}</Paragraph>
-  <Checkbox
-name={'newsletter-subscribe-controlled_${Math.random()}'}
-id={'newsletter-subscribe-controlled_${Math.random()}'}
-isChecked={props.entity.fields.checkboxChecked['it-IT']}
-  />
-</Card>
-: renderDefaultCard(props);
+  sdk={sdk}
+  hasCardEditActions
+  hasCardMoveActions
+  parameters={{
+    instance: {
+      showCreateEntityAction: true,
+      showLinkEntityAction: true,
     }
-}
-viewType='entry'
-isDisabled={fieldControl.disabled}
+  }}
+  renderCustomCard={(props, linkActionsProps, renderDefaultCard) => {
+    return (props.entity.fields.checkboxChecked)
+      ? <Card
+        testId={`custom-card_${Math.random()}`}
+        dragHandleRender={props.renderDragHandle}
+        withDragHandle
+      >
+        <Paragraph>{props.entity.fields.name['it-IT']}</Paragraph>
+        <Checkbox
+          name={`newsletter-subscribe-controlled_${Math.random()}`}
+          id={`newsletter-subscribe-controlled_${Math.random()}`}
+          isChecked={props.entity.fields.checkboxChecked['it-IT']}
+        />
+      </Card>
+      : renderDefaultCard(props);
+  }}
+  viewType='entry'
+  isDisabled={fieldControl.disabled}
 />
